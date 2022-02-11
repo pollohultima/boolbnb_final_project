66a475e3f98bdab99dba5b52a83bf736ccae121d
NicolaL95 <nicola.lilla@gmail.com>
2022-02-11 15:26:35 +0100

    Add seeders to Databaseeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ServiceSeeder::class);
-
+        $this->call(SponsorSeeder::class);
         $this->call(ApartmentSeeder::class);
     }
 }
