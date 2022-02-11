8f9e40a2bc3055c6476614190a5bfeb3d13ddfbb
sciug <gaetano.sciurti@libero.it>
2022-02-11 12:59:50 +0100

    create view table

---
+++ database/factories/ViewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\View;
+use Faker\Generator as Faker;
+
+$factory->define(View::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
