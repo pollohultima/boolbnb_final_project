e8cdf15f04a89521a3991ca0743fc37d82bca60c
NicolaL95 <nicola.lilla@gmail.com>
2022-02-11 22:01:33 +0100

    Add api's Apartments

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -9,6 +9,11 @@ class Apartment extends Model
 {
     protected $fillable = ['title', 'slug', 'rooms', 'bathrooms', 'beds', 'squared_meters', 'address', 'longitude', 'latitiude', 'image', 'is_visible', 'floor', 'price', 'description'];
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function service()
     {
         return $this->belongsToMany(Service::class);
