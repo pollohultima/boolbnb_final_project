42b0b43bdb5b081e04ae71aec3638b7c919e19a4
pollohultima <matteo.polonio@gmail.com>
2022-02-11 16:34:56 +0100

    fix messages and users tables

--- resources/views/guest/apartments/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Index Apartments</h1>
-@endsection
--- resources/views/guest/apartments/show.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Show Apartments</h1>
-@endsection
