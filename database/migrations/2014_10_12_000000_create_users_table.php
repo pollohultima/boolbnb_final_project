7a3ade9355896cbde2503b3cf2b47a14dd6f8344
pollohultima <matteo.polonio@gmail.com>
2022-02-14 12:12:50 +0100

    Changes on register stuff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('lastname');
+            /*    $table->string('lastname');
             $table->date('date_of_birth');
             $table->string('address');
-            $table->string('username');
+            $table->string('username'); */
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
