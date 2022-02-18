177c582e0ecdb7f27314e7502210d67d19ef20e0
sciug <gaetano.sciurti@libero.it>
2022-02-18 14:14:00 +0100

    fix button padding issue

--- public/js/host.js
+++ public/js/host.js
@@ -53506,11 +53506,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\final_project\boolbnb_final_project\resources\js\host.js */"./resources/js/host.js");
-=======
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\boolbnb_final_project\resources\js\host.js */"./resources/js/host.js");
->>>>>>> map_testing
 
 
 /***/ })
