cba46b832e060ac633865e4daa82a69e44ad295a
sciug <gaetano.sciurti@libero.it>
2022-02-18 10:26:57 +0100

    change input color

--- public/js/host.js
+++ public/js/host.js
@@ -53768,11 +53768,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\boolbnb_final_project\resources\js\host.js */"./resources/js/host.js");
-=======
-module.exports = __webpack_require__(/*! /Users/snake/Desktop/Boolean/FINAL_PROJECT/BoolBnb/boolbnb_final_project/resources/js/host.js */"./resources/js/host.js");
->>>>>>> origin/footer
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\final_project\boolbnb_final_project\resources\js\host.js */"./resources/js/host.js");
 
 
 /***/ })
